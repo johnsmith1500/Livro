b321a6c368e6b0463f626173272ee07d1318e0ff
lordmaster69 <guilhermeoliveira33333@gmail.com>
2019-08-15 11:01:23 -0300

    Primeiro save (Livraria)

---
+++ src/livraria/Livro.java
@@ -0,0 +1,31 @@
+package livraria;
+
+public class Livro {
+    private String Titulo;
+    private String Autor;
+    private int ISBN;
+    private int Paginas;
+    private int Ano;
+    private String Editora;
+    private double Valor;
+    
+    public void setTitulo(String titulo){
+     this.Titulo = titulo;
+    }
+    public String getTitulo(){
+        return this.getTitulo();
+    }
+    public void setAutor(String autor){
+     this.Autor = autor;
+    }
+    public String getAutor(String autor){
+    return this.getAutor();
+    }
+    public String setISBN(){
+        this.Titulo = ;
+    }
+     public String getISBN(){
+        return this.getISBN();
+    }
+}
+
