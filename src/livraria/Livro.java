504aab06771681db04d79d6e5ef9682384dc41c6
lordmaster69 <guilhermeoliveira33333@gmail.com>
2019-08-15 12:13:47 -0300

    Interface (Livraria)

--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -18,14 +18,53 @@ public class Livro {
     public void setAutor(String autor){
      this.Autor = autor;
     }
-    public String getAutor(String autor){
-    return this.getAutor();
+    public String getAutor(){
+    return this.getAutor(); 
     }
-    public String setISBN(){
-        this.Titulo = ;
+    public void setISBN(int ISBN){
+     this.ISBN = ISBN;
     }
-     public String getISBN(){
-        return this.getISBN();
+    public int getISBN(){
+    return this.getISBN(); 
+    }
+    public void setPaginas(int Paginas){
+        this.Paginas = Paginas;
+    }
+     public int getPaginas(){
+        return this.getPaginas();
+    }
+     public void setAno(){
+        this.Ano = Ano;
+    }
+     public void setAno(int Ano){
+        this.Ano = Ano;
+    }
+     public void setEditora(String Editora){
+     this.Editora = Editora;
+    }
+    public String getEditora(){
+        return this.getEditora();
+    }
+    public void setValor(double valor){
+     this.Valor = Valor;
+    }
+    public double getValor(){
+        return this.getValor();
+    }
+    
+    public String exibir(){
+        System.out.println("lIVRO:"+Titulo);
+        System.out.println("AUTOR:"+Autor);
+        System.out.println("ISBN:"+ISBN);
+        System.out.println("PAGINAS:"+Paginas);
+        System.out.println("ANO:"+Ano);
+        System.out.println("EDITORA:"+Editora);
+        System.out.println("VALOR:"+Valor);    
+        return null;
+    }
+    public double venda(int qtd){
+        System.out.println("Valor da venda: "+(getValor()*qtd));
+        return qtd;
     }
 }
 
--- src/livraria/Principal.java
+++ src/livraria/Principal.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package livraria;
 
-/**
- *
- * @author Usuário
- */
+
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Livraria i = new Livraria();
+        i.show();
+        
+       
+        Livro a = new Livro();
+        a.setTitulo("Sherlock Homes");
+        a.setAutor("Sir. Arthur Conan Deil");
+        a.setISBN(281964);
+        a.setPaginas(250);
+        a.setAno(1858);
+        a.setEditora("Leiturinha");
+        a.setValor(55.75);
+        a.venda(545);
+        a.exibir();
+        
+        Livro b = new Livro();
+        b.setTitulo("Alice nos pais das maravilhas");
+        b.setAutor("Lewis Carroll");
+        b.setISBN(515705);
+        b.setPaginas(132);
+        b.setAno(1898);
+        b.setEditora("Leiturinha");
+        b.setValor(45.15);
+        b.venda(175);
+        b.exibir();
+        
+        Livro c= new Livro();
+        c.setTitulo("Crime e Castigo");
+        c.setAutor("Fiodor Dostoievski");
+        c.setISBN(173508);
+        c.setPaginas(1626);
+        c.setAno(1878);
+        c.setEditora("Leiturinha");
+        c.setValor(150.15);
+        c.venda(831);
+        c.exibir();
+        
+        Livro d = new Livro();
+        d.setTitulo("Irmãos Karamazov");
+        d.setAutor("Lewis Carroll");
+        d.setISBN(515705);
+        d.setPaginas(132);
+        d.setAno(1898);
+        d.setEditora("Leiturinha");
+        d.setValor(45.15);
+        d.venda(175);
+        d.exibir();
+        
+        /* Livro L = new Livro();
+        L.setTitulo("Que dia");
+        L.exibir();
+        L.setValor(18.00);
+        L.venda(2); */
     }
     
 }
